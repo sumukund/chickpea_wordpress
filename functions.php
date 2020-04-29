d8f55b3a50973c62aebb75bb8eb576e81f1b9e86
sumukund <56210484+sumukund@users.noreply.github.com>
2020-04-29 18:28:31 -0400

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php 
+function learningwordpress_resources() {
+
+    wp_enqueue_style('style', get_stylesheet_uri());
+}
+
+add_action('wp_enqueue_scripts', 'learningwordpress_resources');
+
+// navigation menus
+
+register_nav_menus(array(
+    'primary' => __( 'Primary Menu'),
+    'footer' => __( 'Footer Menu'),
+
+
+));
